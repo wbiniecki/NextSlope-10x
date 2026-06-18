2d09e3814d1e240aa209d9dbcc11ac4fe2fca72a
Wojciech Biniecki <wojciech.biniecki@paramount.com>
2026-06-18 23:59:17 +0200

    test(persistence-migration-baseline): use DockerImageName for Testcontainers Postgres
    Replace the deprecated String constructor on PostgreSQLContainer with
    DockerImageName.parse("postgres:16-alpine") to clear the build deprecation note.

--- src/test/java/com/nextslope/user/UserRepositoryPostgresTests.java
+++ src/test/java/com/nextslope/user/UserRepositoryPostgresTests.java
@@ -11,6 +11,7 @@ import org.springframework.boot.testcontainers.service.connection.ServiceConnect
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
 @Testcontainers
@@ -18,7 +19,7 @@ class UserRepositoryPostgresTests {
 
 	@Container
 	@ServiceConnection
-	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"));
 
 	@Autowired
 	private UserRepository userRepository;
